67bda900d10352f2f3418c62da38b6c1ee040104
f-nakajima <f-nakajima@ite-center.com>
2017-06-09 01:14:58 +0900

    新規作成

---
+++ gamenseni/gamenseni/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gamenseni.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["msg"] = "Hello World !";
+            return View();
+        }
+
+        // GET: Send
+        [HttpPost]
+        public ActionResult Send(string value1)
+        {
+            ViewData["PostData"] = value1 + "を受け取りました。";
+            return View();
+
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ gamenseni/gamenseni/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gamenseni.Startup))]
+namespace gamenseni
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
